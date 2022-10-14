1ba35c52c76d6d86546e83d6c17babea563b5e8e
sena <115485799+wsena02@users.noreply.github.com>
2022-10-15 01:24:15 +0900

    無職転生〜異世界いったら本気出す〜

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("baz").addEventListener("click",function(){
+    alert('飼い主');
+});
+
+document.getElementById('hoge').addEventListener('click',function(){
+    alert('エリス');
+    alert('ルイジェルド');
+})
